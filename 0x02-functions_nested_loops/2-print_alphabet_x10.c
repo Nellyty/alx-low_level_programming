b9b2a754cf6c1987735839a98f8fab6d22c2938b
Nelly Mokaya <nellykemunto001@gmail.com>
2023-06-20 02:49:39 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entity point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z');
+
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
